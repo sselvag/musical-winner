3862c1023ce8ac14cc71d76b32711482d069a71d
sselvag <swaathy.selva@gmail.com>
2022-07-19 22:53:47 -0400

    completed packages and user input questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions for your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please add usage infromation regarding your project here.',
+      },
+      {
+        type: 'input',
+        name: 'testing',
+        message: 'Please add test instructions for your project here.',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please select the relevant license for your project.',
+        choices: ['MIT','GNU','ISC','Other','None']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
