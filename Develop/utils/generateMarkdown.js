a39213c8054aa3bafc4e8ea8d2b324e796826065
sselvag <swaathy.selva@gmail.com>
2022-07-20 21:01:45 -0400

    done generateMarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -25,6 +25,42 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions] (#questions)
+
+  ## Installation
+  To install necessary dependencies, run the following command: ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This project is licensed under the ${renderLicenseSection(data.license)} license.
+  
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  To run tests, run the following command: ${data.testing}
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.github}.
 
 `;
 }
