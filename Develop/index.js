42d8612ae5ceb294236e0ebec8476e4abace65b5
sselvag <swaathy.selva@gmail.com>
2022-07-20 20:18:18 -0400

    updated user input questions and done license functions

--- Develop/index.js
+++ Develop/index.js
@@ -7,52 +7,57 @@ const promptUser = () => {
   return inquirer.prompt ([
     {
       type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a brief description of your project.',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the installation instructions for your project?',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Please add usage infromation regarding your project here.',
-    },
-    {
-      type: 'input',
-      name: 'testing',
-      message: 'Please add test instructions for your project here.',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Please select the relevant license for your project.',
-      choices: ['MIT','GNU','ISC','Other','None']
-    },
-    {
-      type: 'input',
       name: 'github',
-      message: 'What is your github username?',
+      message: 'What is your GitHub username?',
     },
     {
       type: 'input',
       name: 'email',
       message: 'What is your email address?',
     },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a short description of your project.',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license should your project have?',
+      choices: ['MIT','APACHE 2.0', 'GPL 3.0','BSD 3', 'None']
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What command should be run to install dependencies?',
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'What command should be run to run tests?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What does the user need to know about using the repo?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What does the user need to know about contributing to the repo?',
+    }
   ]);
 };
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  retun fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 };
 
 // TODO: Create a function to initialize app
