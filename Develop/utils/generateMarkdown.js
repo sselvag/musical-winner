766bcac2e2b84a38b703a030653f2d5b73ea217c
sselvag <swaathy.selva@gmail.com>
2022-07-26 22:04:27 -0400

    edited website link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -60,9 +60,8 @@ function generateMarkdown(data) {
   To run tests, run the following command: ${data.testing}
 
   ## Questions
-  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.github}.
-  GitHub Profile: https://github.com/${data.github}
-
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. 
+  You can find more of my work at [${data.github}](https://github.com/${data.github}).
 `;
 }
 
